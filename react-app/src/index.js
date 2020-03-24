7520949ef7d8ad787c38268d90988c1b2f491442
rieperr <cihato@sabanciuniv.edu>
2020-03-24 22:48:15 +0100

    Added Counter component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));

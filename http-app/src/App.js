a77a42a0fd5387d22c0708c1c7d41fc1f0005495
rieperr <cihato@sabanciuniv.edu>
2020-03-29 23:52:48 +0200

    Sentry addition

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import http from "./services/httpService";
+import config from "./config.json";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-
-const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 
 class App extends Component {
   state = {
@@ -11,13 +12,13 @@ class App extends Component {
 
   async componentDidMount() {
     //pending > resolved(successs) or rejected(failure)
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(config.apiEndpoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -25,7 +26,7 @@ class App extends Component {
 
   handleUpdate = async post => {
     post.title = "Updated";
-    await axios.put(apiEndpoint + "/" + post.id, post);
+    await http.put(config.apiEndpoint + "/" + post.id, post);
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
     posts[index] = { ...post };
@@ -39,13 +40,10 @@ class App extends Component {
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
     try {
-      await axios.delete(apiEndpoint + "/" + post.id);
+      await http.delete(config.apiEndpoint + "/" + post.id);
     } catch (ex) {
       if (ex.response && ex.response.status === 404) {
         alert("This post has already been deleted");
-      } else {
-        console.log("Logging the error", ex);
-        alert("An unexpected error occurred");
       }
       //Expected (404: not found, 400: bad request) - Client Errors => Display specific error message to the user
       //Unexpected (network down, server down, db down, bug) => log them, display generic and friendly error message
@@ -56,6 +54,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <button className="btn btn-primary" onClick={this.handleAdd}>
           Add
         </button>
---
+++ http-app/src/services/logService.js
@@ -0,0 +1,16 @@
+import * as Sentry from "@sentry/browser";
+
+function init() {
+  Sentry.init({
+    dsn: "https://df3fdf45423943d0b970a07153d5d56d@sentry.io/5179557"
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log
+};
